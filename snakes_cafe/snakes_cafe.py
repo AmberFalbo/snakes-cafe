1ac191ac9c7a573d270860512b2150abb80bdc23
Amber Falbo <amberannfalbo@gmail.com>
2020-12-05 13:30:52 -0800

    initial setup

---
+++ snakes_cafe/snakes_cafe.py
@@ -0,0 +1,21 @@
+# list , dictionary
+
+orders = {
+    'wings': 0,
+}
+
+print(orders)
+
+print('Welcome to Snakes Cafe')
+wing_order = orders['wings']
+
+user_input = input('Please enter a item to order: ')
+
+print(user_input)
+
+print(orders['wings'])
+
+orders['wings'] += 1
+
+print(orders)
+
