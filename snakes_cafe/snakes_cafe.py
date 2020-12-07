eab10f803f0dbe889c90b23b514093642f08e8b7
Amber Falbo <amberannfalbo@gmail.com>
2020-12-06 23:38:20 -0800

    menu solid

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -1,21 +1,75 @@
-# list , dictionary
+print("""
+**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**                                  **
+** To quit at any time, type "quit" **
+**************************************
+""")
 
-orders = {
-    'wings': 0,
-}
+print("""
+Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
 
-print(orders)
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
 
-print('Welcome to Snakes Cafe')
-wing_order = orders['wings']
+Desserts
+--------
+Ice Cream
+Cake
+Pie
 
-user_input = input('Please enter a item to order: ')
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears
+""")
+
+user_input = input("""
+***********************************
+** What would you like to order? **
+***********************************
+> """)
+
+
 
 print(user_input)
 
-print(orders['wings'])
+# list , dictionary
 
-orders['wings'] += 1
+# def snakes():
 
-print(orders)
+#     orders = {
+#         'wings': 0,
+#     }
+    
+#     print(orders)
+    
+#     print('Welcome to Snakes Cafe')
+    
+#     wing_order = orders['wings']
+    
+    
+#     user_input = input('Please enter a item to order: ')
+    
+#     print(user_input)
+    
+#     print(orders['wings'])
+    
+#     orders['wings'] += 1
+    
+    
+#     print(orders)
 
+
+# if __name__ == '__main__':
+#     snakes()
